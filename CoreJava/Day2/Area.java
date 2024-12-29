ca136551fe8c98ba0ebe71608cd401e41c4d0165
ARJUNAN-004 <ceitarjunan25@gmail.com>
2024-12-29 13:12:47 +0530

    Day2

---
+++ CoreJava/Day2/Area.java
@@ -0,0 +1,22 @@
+package DAY02;
+
+public class Area {
+    public double RectArea(double a,double b){
+        return a*b;
+    }
+    public double CircleArea(double r){
+        return Math.PI*r*r;
+    }
+    public double TriArea(double a,double b){
+        return 0.5*a*b;
+    }
+    public static void main(String[] args) {
+        Area area=new Area();
+        double Rectangle=area.RectArea(3,6);
+        double Cicle=area.CircleArea(5);
+        double Triangle=area.TriArea(3,6);
+        System.out.println(Rectangle);
+        System.out.println(Cicle);
+        System.out.println(Triangle);
+    }
+}
